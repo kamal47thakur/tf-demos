630163f88b15c8064880bacbb8ec7f4eebec9588
kamal Kishore <kamal47thakur@gmail.com>
2025-08-07 12:10:55 +0530

    update AMI ID

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
+  ami           = "ami-0b32d400456908bf9" # Replace with a valid AMI ID for your region
   instance_type = "t3.micro"             # Choose your desired instance type
   tags = {
     Name = "MyFirstTerraformEC2"
