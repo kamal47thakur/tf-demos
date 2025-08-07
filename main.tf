6749f3249fc7166839224421b531bed229bca643
kamal47thakur <43463895+kamal47thakur@users.noreply.github.com>
2025-08-07 12:33:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,11 @@
+provider "aws" {
+  region = "ap-south-1" # Specify your desired AWS region
+}
 
-name: Terminate Specific EC2 Instance
-
-on:
-  workflow_dispatch:
-    inputs:
-      instance_id:
-        description: 'i-0b006bedf08ef1745'
-        required: true
-        type: string
-
-jobs:
-  terminate_instance:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Configure AWS Credentials
-        uses: aws-actions/configure-aws-credentials@v4
-        with:
-          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          aws-region: ${{ env.AWS_REGION }} # e.g., us-east-1
-
-      - name: Terminate EC2 Instance
-        run: |
-          aws ec2 terminate-instances --instance-ids ${{ github.event.inputs.instance_id }}
-        env:
-          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          aws-region: ${{ env.AWS_REGION }}
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0b32d400456908bf9" # Replace with a valid AMI ID for your region
+  instance_type = "t3.micro"             # Choose your desired instance type
+  tags = {
+    Name = "Kamal Aws Instance"
+  }
+}
