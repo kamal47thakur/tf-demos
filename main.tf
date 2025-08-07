853f0a6017d5f9c582fc30151e7eaa6b80caa2b4
kamal Kishore <kamal47thakur@gmail.com>
2025-08-07 12:08:14 +0530

    update main

--- main.tf
+++ main.tf
@@ -1,17 +1,12 @@
 
-module "ec2_instance" {
+provider "aws" {
   region = "ap-south-1" # Specify your desired AWS region
-  source  = "terraform-aws-modules/ec2-instance/aws"
+}
 
-  name = "single-instance"
-  ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your regionn
-  instance_type = "t3.micro"
-  key_name      = "kamal.key"
-  monitoring    = true
-  subnet_id     = "subnet-05c286cfb4e752dc1"
-
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
+  instance_type = "t3.micro"             # Choose your desired instance type
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "MyFirstTerraformEC2"
   }
 }
