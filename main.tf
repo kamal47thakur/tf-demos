3a55cad2bbb3dfd78cd302e81d8ae1b96b1a5a6c
kamal Kishore <kamal47thakur@gmail.com>
2025-08-07 12:03:38 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,17 @@
-provider "aws" {
+
+module "ec2_instance" {
   region = "ap-south-1" # Specify your desired AWS region
-}
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
-resource "aws_instance" "my_ec2_instance" {
+  name = "single-instance"
   ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
-  instance_type = "t3.micro"             # Choose your desired instance type
-  tags = {
-    Name = "MyFirstTerraformEC2"
-  }
-}
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "kamal-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  instance_type = "t3.micro"
+  key_name      = "kamal.key"
+  monitoring    = true
+  subnet_id     = "subnet-05c286cfb4e752dc1"
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
