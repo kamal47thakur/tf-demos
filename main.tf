274e146db26f1fc221df43d153027728b1ace808
kamal Kishore <kamal47thakur@gmail.com>
2025-08-08 18:26:26 +0530

    importing jenkins VW

--- main.tf
+++ main.tf
@@ -2,11 +2,7 @@ provider "aws" {
   region = "ap-south-1" # Specify your desired AWS region
   profile = "default"
   }
-
-resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0b32d400456908bf9" # Replace with a valid AMI ID for your region
-  instance_type = "t3.micro"              # Choose your desired instance type
-  tags = {
-    Name = "Kamal Aws Instance"
-  }
+resource "aws_instance" "jenkins" {
+ ami           = "unknown"
+ instance_type = "unknown"
 }
