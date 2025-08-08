add8a885b1e72c7b56cb81c0e7d510a46dc8f091
kamal Kishore <kamal47thakur@gmail.com>
2025-08-08 19:57:14 +0530

    Adding VPC PWIMS-VPC01

--- vpc.tf
+++ vpc.tf
@@ -2,5 +2,6 @@
       cidr_block = "10.0.0.0/16" # Replace with your VPC's CIDR block
       tags = {
         Name = "PWIMS-VPC01" # Replace with your VPC's name tag
+        Environment = "Production"
       }
     }
