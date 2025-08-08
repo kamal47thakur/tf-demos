03a52928b09ae86cac6dad19afcb37242b7e3a6b
kamal Kishore <kamal47thakur@gmail.com>
2025-08-08 19:49:38 +0530

    importing VPC PWIMS-VPC01

---
+++ vpc.tf
@@ -0,0 +1,6 @@
+     resource "aws_vpc" "PWIMS-VPC01" {
+      cidr_block = "10.0.0.0/16" # Replace with your VPC's CIDR block
+      tags = {
+        Name = "PWIMS-VPC01" # Replace with your VPC's name tag
+      }
+    }
