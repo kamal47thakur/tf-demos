32d20d591e5d01d0606be7ffc8506d1303348bf6
kamal Kishore <kamal47thakur@gmail.com>
2025-08-08 19:31:59 +0530

    building new Instance

--- main.tf
+++ main.tf
@@ -9,9 +9,3 @@ resource "aws_instance" "jenkins" {
      "Name": "jenkins"
  }
 }
-resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0b32d400456908bf9" # Replace with a valid AMI ID for your region
-  instance_type = "t3.micro"              # Choose your desired instance type
-  tags = {
-    Name = "Kamal Aws Instance"
-  }
