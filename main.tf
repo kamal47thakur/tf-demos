ff76f4bd83e9bc0f99b03440bc14ecc2db23c2db
kamal Kishore <kamal47thakur@gmail.com>
2025-08-08 18:19:48 +0530

    add credentials file

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region = "ap-south-1" # Specify your desired AWS region
-  #profile = "default"
-  shared_credentials_files = ["%USERPROFILE%\\.aws\\credentials"]
-}
+  profile = "default"
+  }
 
 resource "aws_instance" "my_ec2_instance" {
   ami           = "ami-0b32d400456908bf9" # Replace with a valid AMI ID for your region
