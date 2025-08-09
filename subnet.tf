d01f1366a71e6603abf9dceec23d33998131986d
kamal Kishore <kamal47thakur@gmail.com>
2025-08-09 12:05:25 +0530

    import subnet PWIMS public and private

---
+++ subnet.tf
@@ -0,0 +1,16 @@
+ resource "aws_subnet" "PWIMS-PRIVATE" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "PWIMS-PRIVATE"
+  }
+}
+ resource "aws_subnet" "PWIMS-PUBLIC" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "PWIMS-PUBLIC"
+  }
+
