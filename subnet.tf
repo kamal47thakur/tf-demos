94b57337626079d2a6094ab032c181acae0e58d4
kamal Kishore <kamal47thakur@gmail.com>
2025-08-09 12:14:16 +0530

    import subnet PWIMS public and private

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,9 @@
- resource "aws_subnet" "PWIMS-PRIVATE" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "PWIMS-PRIVATE"
-  }
-}
+    resource "aws_subnet" "PWIMS-PRIVATE" {
+      vpc_id     = "vpc-0d5681e896c5a5820" # Replace with your VPC ID
+      cidr_block = "10.0.1.0/24"          # Replace with the subnet's CIDR block
+      tags = {
+        Name = "PWIMS-PRIVATE" # Replace with existing tags
+      }
+    }
 
